96273663842805783e263e0a43b19e71b4d4c825
Aditya Sood <adityasood314@gmail.com>
2023-01-10 10:13:02 +0530

    Added Splash Screen

--- app/src/main/java/com/app/newzpaper/SplashActivity.kt
+++ app/src/main/java/com/app/newzpaper/SplashActivity.kt
@@ -1,11 +1,30 @@
 package com.app.newzpaper
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.concurrent.thread
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val thread = Thread(){
+            kotlin.run {
+                try {
+                    Thread.sleep(4000)
+                }
+                catch (e:Exception){
+                    e.printStackTrace()
+
+                }
+                finally {
+                    val intent = Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                }
+
+            }
+        }
+        thread.start()
     }
 }
